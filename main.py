2b957d5321cfe0a45965da8590238e8e30d55a9a
Graysander <maxim.cuynat@viacesi.fr>
2025-01-19 21:06:21 +0100

    Modifs de merde

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pprint
 from Assembler import Assembler
 # Example usage
-assembler = Assembler("program.asm", "program.txt")
-assembler.compile_assembly()
+assembler = Assembler("input.asm", "output.bin")
